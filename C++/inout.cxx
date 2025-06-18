8a07328db1ecd8e2371180925580d4b4e5a26212
JuanLopez2004 <108855394+JuanLopez2004@users.noreply.github.com>
2025-06-17 19:57:36 -0500

    cxx file

---
+++ C++/inout.cxx
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    int n, n2, n3;
+    cin >> n >> n2 >> n3;
+    n = n + n2 + n3;
+    cout << n << endl;
+    return 0;
+}
